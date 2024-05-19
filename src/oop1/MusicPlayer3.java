9ff24df339fe52af6480d6da15529847e89009b7
WIE HYANG HOON <gidgns1995@gmail.com>
2024-05-20 00:11:54 +0900

    [add] 절차 지향 프로그래밍 - 메서드 추출전 클래스 추가

--- src/oop1/MusicPlayer3.java
+++ src/oop1/MusicPlayer3.java
@@ -1,20 +1,40 @@
 package oop1;
 
-public class MusicPlayer2 {
+public class MusicPlayer3 {
     public static void main(String[] args) {
         // 인스턴스(객체) 생성
         MusicPlayerData data = new MusicPlayerData();
 
         // 음악 플레이어 켜기
+        on(data);
+        // 볼륨 증가
+        volumeUp(data);
+        // 볼륨 감소
+        volumeDown(data);
+        // 음악 플레이어 상태
+        showStatus(data);
+        // 음악 플레이어 끄기
+        off(data);
+    }
+
+    static void on(MusicPlayerData data){
         data.isOn = true;
         System.out.println("음악 플레이어를 시작합니다.");
-        // 볼륨 증가
+    }
+    static void off(MusicPlayerData data) {
+        data.isOn = false;
+        System.out.println("음악 플레이어를 종료합니다.");
+    }
+    static void volumeUp(MusicPlayerData data){
         data.volume++;
         System.out.println("음악 플레이어 볼륨 : " + data.volume);
-        // 볼륨 감소
+    }
+    static void volumeDown(MusicPlayerData data){
         data.volume--;
         System.out.println("음악 플레이어 볼륨 : " + data.volume);
-        // 음악 플레이어 상태
+    }
+
+    static void showStatus(MusicPlayerData data){
         System.out.println("음악 플레이어 상태 확인");
 
         if(data.isOn){
@@ -22,9 +42,5 @@ public class MusicPlayer2 {
         } else {
             System.out.println("음악 플레이어 OFF");
         }
-
-        // 음악 플레이어 끄기
-        data.isOn = false;
-        System.out.println("음악 플레이어를 종료합니다.");
     }
 }
