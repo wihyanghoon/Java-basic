abb8a2e50b96585dc2916e70df91c543374e1f45
WIE HYANG HOON <gidgns1995@gmail.com>
2024-05-20 16:47:44 +0900

    [modify] class add method

---
+++ src/oop1/ValueDataMain.java
@@ -0,0 +1,16 @@
+package oop1;
+
+public class ValueDataMain {
+    public static void main(String[] args) {
+        ValueData valueData = new ValueData();
+
+        add(valueData);
+        add(valueData);
+        add(valueData);
+    }
+
+    static void add (ValueData valueData) {
+     valueData.value++;
+        System.out.println("ValueData value = " + valueData.value);
+    }
+}
