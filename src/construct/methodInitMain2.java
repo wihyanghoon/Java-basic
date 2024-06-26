b911939e236ad695acf1cc8079ac15490160cc4b
WIE HYANG HOON <gidgns1995@gmail.com>
2024-06-26 21:11:20 +0900

    [add] 객체 지향 프로그래밍 - construct 필요성

--- src/construct/methodInitMain2.java
+++ src/construct/methodInitMain2.java
@@ -3,21 +3,19 @@ package construct;
 public class methodInitMain2 {
     public static void main(String[] args) {
         MemberInit member = new MemberInit();
-
-        member.name = "wi hyanng hoon";
-        member.age = 18;
-        member.gread = 80;
-
         MemberInit member2 = new MemberInit();
-
-        member2.name = "wi een kyung";
-        member2.age = 18;
-        member2.gread = 80;
+        InitMember(member, "wi", 30, 80);
+        InitMember(member2, "hoon", 30, 80);
 
         MemberInit[] members = {member, member2};
-
         for (MemberInit memberInit : members) {
             System.out.println(memberInit.name);
         }
+    }
+
+    static void InitMember (MemberInit member, String name, int age, int gread) {
+        member.name = name;
+        member.age = age;
+        member.gread = gread;
     }
 }
