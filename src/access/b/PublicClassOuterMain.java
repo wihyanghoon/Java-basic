2271bca7d9f9f2bc462d1c192026ef8ec4f45e71
WIE HYANG HOON <gidgns1995@gmail.com>
2024-07-04 00:21:35 +0900

    [add] 객체 지향 프로그래밍 - 접근지정자, class level

--- src/access/b/PublicClassOuterMain.java
+++ src/access/b/PublicClassOuterMain.java
@@ -9,7 +9,7 @@ public class PublicClassOuterMain {
         PublicCalss publicCalss = new PublicCalss();
 
 
-        // default 레벨의 클래스이기 때문에 다른 패키지에서 호출 할 수 없음
+        //default 레벨의 클래스이기 때문에 다른 패키지에서 호출 할 수 없음
         //DefaultClass defaultClass = new DefaultClass();
         //DefaultClass2 defaultClass2 = new DefaultClass2();
     }
